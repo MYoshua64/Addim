0bfd2145526895ea73f35aee29cdfab13f1f1f76
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2019-12-12 01:27:02 +0200

    Board building

---
+++ Addim/Assets/Tile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Text tileNumberUpper;
+    [SerializeField] Text tileNumberLower;
+    public int x { get; set; }
+    public int y { get; set; }
+    Color tileColor;
+    public Color color
+    {
+        get
+        {
+            return tileColor;
+        }
+        set
+        {
+            tileColor = value;
+            GetComponent<SpriteRenderer>().color = value;
+        }
+    }
+    int number;
+    public int Number
+    {
+        get
+        {
+            return number;
+        }
+        set
+        {
+            number = value;
+            tileNumberUpper.text = tileNumberLower.text = value.ToString();
+        }
+    }
+
+    private void Awake()
+    {
+        x = Mathf.FloorToInt(transform.position.x);
+        y = Mathf.FloorToInt(transform.position.y);
+    }
+}
