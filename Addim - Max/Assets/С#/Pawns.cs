2ee4f749560325f6ea02953a5ff9e5b3e2169bab
MaxSabirov <53007578+MaxSabirov@users.noreply.github.com>
2019-12-18 18:59:19 +0200

    Dice and Timer

---
+++ Addim - Max/Assets/С#/Pawns.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawns : MonoBehaviour
+{
+    public GameObject [] PawnsGreen = new GameObject[4];
+    public GameObject [] PawnsRed = new GameObject[4];
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
